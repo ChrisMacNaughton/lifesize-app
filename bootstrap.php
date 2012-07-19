11ca8159fe97cf3efb3142826454936c7f51080d
IceyEC <chmacnaughton@gmail.com>
2012-07-19 15:09:39 -0500

    working on it for local/remote dev

--- bootstrap.php
+++ bootstrap.php
@@ -1,14 +1,25 @@
 <?php
+error_reporting(E_ALL);
 define('START_TIME', microtime());
 require_once 'vendor/autoload.php';
 require_once 'classes/User.php';
 require_once 'classes/Uri.php';
+
+if (get_cfg_var('aws.access_key') === false) {
 $options = array(
-	'certificate_auithority'->get_cfg_var('aws.param1'),
+	'certificate_authority'=>true,
+	'default_cache_config' => '',
+	'key' => 'AKIAIZCMBC2UFLIFHU2Q',
+	'secret' => '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',
+);
+} else {
+$options = array(
+	'certificate_authority'=>get_cfg_var('aws.param1'),
 	'default_cache_config' => '',
 	'key' => get_cfg_var('aws.access_key'),
 	'secret' => get_cfg_var('aws.secret_key'),
 );
+}
 $dynamodb = new AmazonDynamoDB($options);
 
 // Instantiate, configure, and register the session handler
