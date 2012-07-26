956ea6c2908634c5efee18233aee44808f405bd1
IceyEC <chmacnaughton@gmail.com>
2012-07-26 09:40:45 -0500

    testing sending mail on aws

--- app/classes/user.php
+++ app/classes/user.php
@@ -24,18 +24,41 @@ protected $company = array();
 		}
 	}
 	public function register($user) {
-		global $options;
+		global $smtpSettings;
 		$password = substr(md5(hash('sha512', rand(1,1000))), 0, 10);
 		$loginUrl =  'http://' . PATH . '/user/login';
 		$signature = 'The ' . COMPANY_NAME . ' Team';
 		$message = sprintf("Hello, %s\n\nYour login details are: \n\nEmail: %s\nPassword: %s\n\nYou will be required to change this after your first login.\n\nPlease visit %s to login!\n\nIf the link above doesn't work, copy this link into your browser: %s\n\nThank you!\n%s", $user[':user_name'], $user[':email'],$password,'<a href="' . $loginUrl . '">Login</a>', $loginUrl, $signature);
 		
-		//echo $message;
-		$destination = $user[':email'];
-		$source = "chmacnaughton@gmail.com";
-		//use amazon email function (amazon SES) to send registration email
+		$from = COMPANY_NAME . " <chmacnaughton@gmail.com>";
+		$to = $user[':name'] . " <" . $user[":email"] . ">";
 		
-		echo "Source: $source<br />Destination: $destination<br /><br />$message";
+		$subject = COMPANY_NAME . " Registration";
+		
+		$host = $smtpSettings['server'];
+		$username = $smtpSettings['username'];
+		$password = $smtpSettings['password'];
+		
+		$headers = array(
+			'From'=>$from,
+			'To'=>$to,
+			'Subject'=>$subject
+		);
+		$port = "465";
+		$smtp = Mail::factory('smtp',
+			array ('host' => 'ssl://'.$host,
+				'auth' => true,
+				'port'=>$port,
+				'username' => $username,
+				'password' => $password
+			));
+			
+		$mail = $smtp->send($to, $headers, $body);
+		if (PEAR::isError($mail)) {
+			echo("<p>" . $mail->getMessage() . "</p>");
+		} else {
+			echo("<p>Message successfully sent!</p>");
+		}
 		
 	}
 	public function login($email, $password) {
