aa6af361409a800d8cf0f85fa85adf0c21177f2a
IceyEC <chmacnaughton@gmail.com>
2012-08-03 11:39:23 -0500

    found the header

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-echo "<!--";print_r($_SERVER); echo "-->";
+echo "<!--";print_r($_SERVER['HTTP_X_FORWARDED_PROTO']); echo "-->";
 $app['start'] = microtime();
 define("COMPANY_NAME", 'VC-Control');
 include 'bootstrap.php';
