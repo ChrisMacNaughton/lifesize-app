935f0a0ad7e0f3f23ad0729fbd27b3ffdeb7733f
IceyEC <chmacnaughton@gmail.com>
2012-08-29 14:54:08 -0500

    removign requirement for enabled for total alarms

--- app/controllers/defaultController.php
+++ app/controllers/defaultController.php
@@ -7,7 +7,7 @@ class defaultController extends Controller {
 	}
 	public function indexAction() {
 		$data['title'] = "Dashboard";
-		$stmt = $this->db->prepare("SELECT count(*) AS count FROM devices_alarms INNER JOIN users WHERE company_id = :company AND enabled = 1 AND active = 1");
+		$stmt = $this->db->prepare("SELECT count(*) AS count FROM devices_alarms INNER JOIN users WHERE company_id = :company AND active = 1");
 		$stmt->execute(array(':company'=>$this->company['id']));
 		$res = $stmt->fetch(PDO::FETCH_ASSOC);
 		$data['active_alarms'] = $res['count'];
