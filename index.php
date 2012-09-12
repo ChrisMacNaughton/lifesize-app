3ef19ecd16b7fa06c2e13742811acec81b9ac56b
Chris MacNaughton <chris@control.vc>
2012-09-12 08:23:10 -0500

    adding updater to main app

--- index.php
+++ index.php
@@ -1,10 +1,24 @@
 <?php
 
 $app['start'] = microtime();
+
+
+
 define("COMPANY_NAME", 'ControlVC');
 include 'bootstrap.php';
 if ($_SERVER['HTTP_X_FORWARDED_PROTO'] == "http")
 	header("Location: https://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, PATH . '/updater/update.php');
+curl_setopt($ch, CURLOPT_TIMEOUT_MS, 1);
+curl_setopt($ch, CURLOPT_RETURN_TRANSFER, true);
+
+curl_exec($ch);
+curl_setopt($ch, CURLOPT_URL, PATH . '/updater/maintainer.php');
+curl_exec($ch);
+
+
 $user = new User();
 $ctrl = ($uri->seg(0) == '' || $uri->seg[0] == 'home') ? 'default' : $uri->seg(0);
 $actn =  ($uri->seg(1) == '') ? 'index' : $uri->seg(1);
