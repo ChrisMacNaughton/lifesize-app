0ed9c2736c35b15736271cb0b36f4e63376548f0
Chris MacNaughton <chris@control.vc>
2012-09-17 08:51:33 -0500

    working on integrating newrelic into the main app more

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
 $arr = array();
 $app['start'] = microtime();
-
+if(function_exists(newrelic_disable_autorun)){
+	define('NEW_RELIC', true);
+} else {
+	define("NEW_RELIC", false);
+}
 define("COMPANY_NAME", 'ControlVC');
 include 'bootstrap.php';
 if ($_SERVER['HTTP_X_FORWARDED_PROTO'] == "http")
@@ -39,6 +43,8 @@ $redir = true;
 	
 	if ($redir)
 	{
+		if (NEW_RELIC)
+			newrelic_ignore_transaction();
 		if(!array_search(l("error_need_to_login"), $_SESSION['flash']) && ($ctrl != 'default' && $actn != 'index'))
 		$_SESSION['flash'][] = l("error_need_to_login");
 		session_write_close();
@@ -62,4 +68,6 @@ if (method_exists($controller, $actionName)) {
 	$controller = new errorController();
 	$controller->errorAction($ctrl, $actn);
 }
+if (NEW_RELIC)
+			newrelic_name_transaction($controllerName . '/' . $actionName);
 $db = null;
