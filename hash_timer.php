e9b1c2e10b08eec2ceaa1da915dc16f8b3789496
Chris MacNaughton <chris@control.vc>
2012-09-21 13:02:13 -0500

    tweaking

--- hash_timer.php
+++ hash_timer.php
@@ -1,16 +1,17 @@
 <?php
 include 'app/classes/passwordhash.php';
+set_time_limit(0);
 $random_pass = substr(sha1(microtime(true) . rand(1,1000)), 2, 10);
 header("Content-Type: text/plain");
 echo "pass used for test: $random_pass\n\n";
-for ($i=1;$i<18;$i++){
+for ($i=1;$i<16;$i++){
 	$hasher = new PasswordHash($i,false);
 	$start = microtime(true);
-	$hasher->HashPAssword($random_pass);
+	$hasher->HashPassword($random_pass);
 	$final = microtime(true) - $start;
-	echo "For $i iterations: " . $final . "ms\n";
-	ob_flush();
-	if($final/1000 > 10) break;
+	echo "For ".pow(2,$i)." iterations($i): " . round($final*1000, 5) . "ms\n";
+	flush();$hasher = null;
+	if($final > 30) break;
 }
 
 echo "It took more than 10 seconds for the last iteration";
