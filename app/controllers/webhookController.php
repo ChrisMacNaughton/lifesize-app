7dace9930c6948b6b23d73e39dd678a13e82d49b
Chris MacNaughton <chris@control.vc>
2012-10-09 08:20:22 -0500

    working on recieving webhooks

---
+++ app/controllers/webhookController.php
@@ -0,0 +1,13 @@
+<?php
+
+public function webhookController Extends Controller{
+	public function indexAction(){
+		$stmt = $this->db->prepare("INSERT INTO webhooks (id, hook) VALUES (:id, :hook)");
+		$id = 'webhook-' . substr(sha1(rand(1,1000).microtime(true)),0,10);
+		$hook = json_encode($_POST);
+
+		$stmt->execute(array(':id'=>$id,':hook'=>$hook));
+
+		header("HTTP/1.0 200 OK");
+	}	
+}
