67778be5196d48af14ef37dad875b8a641325140
Chris MacNaughton <chris@control.vc>
2012-10-09 08:28:58 -0500

    adding request body

--- app/controllers/webhookController.php
+++ app/controllers/webhookController.php
@@ -4,7 +4,11 @@ class webhookController Extends Controller{
 	public function indexAction(){
 		$stmt = $this->db->prepare("INSERT INTO webhooks (id, hook) VALUES (:id, :hook)");
 		$id = 'webhook-' . substr(sha1(rand(1,1000).microtime(true)),0,10);
-		$hook = json_encode(array("Post"=>$_POST, "Get"=>$_GET, "Request"=>$_REQUEST));
+
+		$body = @file_get_contents('php://input');
+		$event_json = json_decode($body, true);
+
+		$hook = json_encode(array("Post"=>$_POST, "Get"=>$_GET, "Request"=>$_REQUEST, "Body"=>$event_json));
 
 		$stmt->execute(array(':id'=>$id,':hook'=>$hook));
 
